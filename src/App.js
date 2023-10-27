029df396d77ca7cde88fde6624895852029fe010
Jenna Westendorf <jenna.westendorf@gmail.com>
2023-10-27 13:32:18 -0700

    Added Dictionary component with form element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'></header>
+    <div className='container'>
+      <div className='App'>
+        <header className='App-header'></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className='text-center'>
+          Open Source Code by Jenna Westendorf
+        </footer>
+      </div>
     </div>
   );
 }

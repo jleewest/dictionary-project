a966a1ca5fc382e807cbb950ca332d5e29bf014d
Jenna Westendorf <jenna.westendorf@gmail.com>
2023-10-28 14:26:40 -0700

    Rendered photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,19 +2,31 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './Dictionary.css';
 import Results from './Results';
+import Photos from './Photos';
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState('');
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      'zU0pdCSSwxdRsuxhpPmK1eVFsdcj4ZhAY2KmdMIvKqVGC4GRzcXhNOYE';
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -33,6 +45,7 @@ export default function Dictionary() {
           />
         </form>
         <Results results={results} />
+        <Photos photos={photos} />
       </section>
     </div>
   );
